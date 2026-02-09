86af04e253ab5909a41f3092a1def458e0011402
Happy-negi238 <happynegi238@gmail.com>
2026-02-09 08:20:28 +0530

    tax deduct

--- src/08-tax-calculator.js
+++ src/08-tax-calculator.js
@@ -27,4 +27,21 @@
  */
 export function calculateTax(income) {
   // Your code here
+  let tax = 0;
+  if(income<0 || income === 0){
+    return 0;
+  }else{
+    if(income>0 && income <= 10000){
+      return 0;
+    }else if(income>10000 && income<=30000) {
+      tax = (income - 10000) * 10/100;
+      return tax;
+    }else if(income>30000 && income <= 70000){
+      tax = 2000 + (income - 30000) * 20/100;
+      return tax;
+    }else if(income>70000){
+      tax = 2000 + 8000 + (income - 70000) * 30/100;
+      return tax;
+    }
+  }
 }
